98f4a4d4c11a6453344c9ad3d70ac46d448c0ffc
mmerle <35971019+mmerle@users.noreply.github.com>
2024-11-19 15:16:22 -0500

    cleanup

--- sanity.cli.js
+++ sanity.cli.js
@@ -1,8 +1,4 @@
-/**
- * This configuration file lets you run `$ sanity [command]` in this folder
- * Go to https://www.sanity.io/docs/cli to learn more.
- **/
 import { defineCliConfig } from 'sanity/cli';
-import { dataset, projectId } from '~/lib/sanity/api';
+import { dataset, projectId } from './src/lib/sanity/api.js';
 
 export default defineCliConfig({ api: { projectId, dataset } });
